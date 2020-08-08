51dbbf75f49561d22380687e978e79975fcca7aa
costanzopa <costanzopa@gmail.com>
2020-08-08 15:23:37 -0300

    Adding admin side and top bars

---
+++ src/components/Admin/MenuTopBar/MenuTopBar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  MenuUnfoldOutlined,
+  PoweroffOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons';
+import { Button } from 'antd';
+import AppLogo from '../../../assets/png/app-logo.png';
+
+import './MenuTopBar.scss';
+
+export default function MenuTopBar(props) {
+  const { menuCollapsed, setMenuCollapsed } = props;
+
+  return (
+    <div className="menu-top-bar">
+      <div className="menu-top-bar__left">
+        <img src={AppLogo} alt="Pablo Agustin Costanzo" />
+        <Button
+          type="link"
+          onClick={() => setMenuCollapsed(!menuCollapsed)}
+          icon={menuCollapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+        />
+      </div>
+      <div className="menu-top-bar__right">
+        <Button
+          type="link"
+          onClick={() => console.log('Hola')}
+          icon={<PoweroffOutlined />}
+        />
+      </div>
+    </div>
+  );
+}
--- src/layouts/LayoutAdmin.js
+++ src/layouts/LayoutAdmin.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Layout } from 'antd';
 import LayoutRoutes from './LayoutRoutes.js';
 
 import './LayoutAdmin.scss';
+import MenuTopBar from '../components/Admin/MenuTopBar';
+import MenuSider from '../components/Admin/MenuSider';
 
 const LayoutAdmin = (props) => {
   const { routes } = props;
+  const [menuCollapsed, setMenuCollapsed] = useState(false);
   const { Header, Content, Footer } = Layout;
 
   return (
     <Layout>
-      <h2>Menu Sider Admin</h2>
-      <Layout>
-        <Header>Header</Header>
-        <Content>
+      <Layout
+        className="layout-admin"
+        style={{ marginLeft: menuCollapsed ? '80px' : '200px' }}
+      >
+        <MenuSider menuCollapsed={menuCollapsed} />
+        <Header className="header">
+          <MenuTopBar
+            menuCollapsed={menuCollapsed}
+            setMenuCollapsed={setMenuCollapsed}
+          />
+        </Header>
+        <Content className="content">
           <LayoutRoutes routes={routes} />
         </Content>
-        <Footer>Pablo Costanzo</Footer>
+        <Footer className="footer">Pablo Costanzo</Footer>
       </Layout>
     </Layout>
   );
