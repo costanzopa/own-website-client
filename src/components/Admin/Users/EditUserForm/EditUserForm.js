fc6282e800d4985ea08ac599678e319d20a76595
costanzopa <costanzopa@gmail.com>
2020-08-12 16:51:17 -0300

    Adding edit modal and dropzone

---
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -0,0 +1,179 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import {
+  Avatar,
+  Form,
+  Input,
+  Select,
+  Button,
+  Row,
+  Col,
+  notification,
+} from 'antd';
+import { MailOutlined, UserOutlined, LockOutlined } from '@ant-design/icons';
+import { useDropzone } from 'react-dropzone';
+import NoAvatar from '../../../../assets/img/png/no-avatar.png';
+import './EditUserForm.scss';
+
+const EditUserForm = (props) => {
+  const { user, setIsVisibleModal } = props;
+  const [avatar, setAvatar] = useState(null);
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    setUserData({
+      name: user.name,
+      lastname: user.lastname,
+      email: user.email,
+      role: user.role,
+      avatar: user.avatar,
+    });
+  }, [user]);
+
+  return (
+    <div className="edit-user-form">
+      <UploadAvatar avatar={avatar} setAvatar={setAvatar} />
+      <EditForm userData={userData} setUserData={setUserData} />
+    </div>
+  );
+};
+
+function UploadAvatar(props) {
+  const { avatar, setAvatar } = props;
+  const [avatarUrl, setAvatarUrl] = useState(null);
+
+  useEffect(() => {
+    if (avatar) {
+      if (avatar.preview) {
+        setAvatarUrl(avatar.preview);
+      } else {
+        setAvatarUrl(avatar);
+      }
+    } else {
+      setAvatarUrl(null);
+    }
+  }, [avatar]);
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      setAvatar({ file, preview: URL.createObjectURL(file) });
+    },
+    [setAvatar]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: 'image/jpeg, image/png',
+    noKeyboard: true,
+    onDrop,
+  });
+
+  return (
+    <div className="upload-avatar" {...getRootProps()}>
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <Avatar size={150} src={NoAvatar} />
+      ) : (
+        <Avatar size={150} src={avatarUrl ? avatarUrl : NoAvatar} />
+      )}
+    </div>
+  );
+}
+
+function EditForm(props) {
+  const { userData, setUserData, updateUser } = props;
+  const { Option } = Select;
+
+  return (
+    <Form className="form-edit" onSubmit={updateUser}>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Nombre"
+              value={userData.name}
+              onChange={(e) =>
+                setUserData({ ...userData, name: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="Apellidos"
+              value={userData.lastname}
+              onChange={(e) =>
+                setUserData({ ...userData, lastname: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<MailOutlined />}
+              placeholder="Correo electronico"
+              value={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item>
+            <Select
+              placeholder="Seleccióna una rol"
+              onChange={(e) => setUserData({ ...userData, role: e })}
+              value={userData.role}
+            >
+              <Option value="admin">Administrador</Option>
+              <Option value="editor">Editor</Option>
+              <Option value="reviewr">Revisor</Option>
+            </Select>
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row gutter={24}>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Contraseña"
+              onChange={(e) =>
+                setUserData({ ...userData, password: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+        <Col span={12}>
+          <Form.Item>
+            <Input
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Repetir contraseña"
+              onChange={(e) =>
+                setUserData({ ...userData, repeatPassword: e.target.value })
+              }
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="btn-submit">
+          Actualizar Usuario
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default EditUserForm;
--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import {
-  Switch,
-  List,
-  Avatar,
-  Button,
-  Modal as ModalAntd,
-  notification,
-} from "antd";
+import React, { useState, useEffect } from 'react';
+import { Switch, List, Avatar, Button } from 'antd';
 import {
   EditOutlined,
   StopOutlined,
   DeleteOutlined,
   CheckOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons';
 
-import "./ListUsers.scss";
+import './ListUsers.scss';
 
-import NoAvatar from "../../../../assets/img/png/no-avatar.png";
+import NoAvatar from '../../../../assets/img/png/no-avatar.png';
+import Modal from '../../../../components/Modal';
+import EditUserForm from '../EditUserForm';
+
 export default function ListUsers(props) {
   const { usersActive, usersInactive } = props;
   const [viewUsersActives, setViewUsersActives] = useState(true);
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalContent, setModalContent] = useState(null);
+
   return (
     <div className="list-users">
       <div className="list-users__header">
@@ -29,12 +29,27 @@ export default function ListUsers(props) {
             onChange={() => setViewUsersActives(!viewUsersActives)}
           />
           <span>
-            {viewUsersActives ? "Usuarios Activos" : "Usuarios Inactivos"}
+            {viewUsersActives ? 'Usuarios Activos' : 'Usuarios Inactivos'}
           </span>
         </div>
-        {viewUsersActives
-          ? <UsersActive usersActive={usersActive} />
-          : <UsersInactive usersInactive={usersInactive} />}
+        {viewUsersActives ? (
+          <UsersActive
+            usersActive={usersActive}
+            setIsVisibleModal={setIsVisibleModal}
+            setModalTitle={setModalTitle}
+            setModalContent={setModalContent}
+          />
+        ) : (
+          <UsersInactive usersInactive={usersInactive} />
+        )}
+
+        <Modal
+          title={modalTitle}
+          isVisible={isVisibleModal}
+          setIsVisible={setIsVisibleModal}
+        >
+          {modalContent}
+        </Modal>
       </div>
     </div>
   );
@@ -43,66 +58,84 @@ export default function ListUsers(props) {
 const UsersActive = (props) => {
   const {
     usersActive,
+    setIsVisibleModal,
+    setModalTitle,
+    setModalContent,
   } = props;
-  return (<List
-    className="users-active"
-    itemLayout="horizontal"
-    dataSource={usersActive}
-    renderItem={(user) => (
-      <List.Item
-        actions={[
-          <Button type="primary">
-            <EditOutlined />
-          </Button>,
-          <Button type="danger">
-            <StopOutlined />
-          </Button>,
-          <Button type="danger">
-            <DeleteOutlined />
-          </Button>,
-        ]}
-      >
-        <List.Item.Meta
-          avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
-          title={`
-            ${user.name ? user.name : "..."}
-            ${user.lastname ? user.lastname : "..."}
+
+  const editUser = (user) => {
+    setIsVisibleModal(true);
+    setModalTitle(
+      `Editar ${user.name ? user.name : '...'} ${
+        user.lastname ? user.lastname : '...'
+      }`
+    );
+    setModalContent(
+      <EditUserForm user={user} setIsVisibleModal={setIsVisibleModal} />
+    );
+  };
+
+  return (
+    <List
+      className="users-active"
+      itemLayout="horizontal"
+      dataSource={usersActive}
+      renderItem={(user) => (
+        <List.Item
+          actions={[
+            <Button type="primary" onClick={() => editUser(user)}>
+              <EditOutlined />
+            </Button>,
+            <Button type="danger">
+              <StopOutlined />
+            </Button>,
+            <Button type="danger">
+              <DeleteOutlined />
+            </Button>,
+          ]}
+        >
+          <List.Item.Meta
+            avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
+            title={`
+            ${user.name ? user.name : '...'}
+            ${user.lastname ? user.lastname : '...'}
           `}
-          description={user.email}
-        />
-      </List.Item>
-    )}
-  />);
+            description={user.email}
+          />
+        </List.Item>
+      )}
+    />
+  );
 };
 
 const UsersInactive = (props) => {
-  const {
-    usersInactive,
-  } = props;
-  return (<List
-    className="users-active"
-    itemLayout="horizontal"
-    dataSource={usersInactive}
-    renderItem={(user) => (
-      <List.Item
-        actions={[
-          <Button type="primary">
-            <CheckOutlined />
-          </Button>,
-          <Button type="danger">
-            <DeleteOutlined />
-          </Button>,
-        ]}
-      >
-        <List.Item.Meta
-          avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
-          title={`
-                ${user.name ? user.name : "..."}
-                ${user.lastname ? user.lastname : "..."}
+  const { usersInactive } = props;
+  return (
+    <List
+      className="users-active"
+      itemLayout="horizontal"
+      dataSource={usersInactive}
+      renderItem={(user) => (
+        <List.Item
+          actions={[
+            <Button type="primary">
+              <CheckOutlined />
+            </Button>,
+            <Button type="danger">
+              <DeleteOutlined />
+            </Button>,
+          ]}
+        >
+          <List.Item.Meta
+            avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
+            title={`
+                ${user.name ? user.name : '...'}
+                ${user.lastname ? user.lastname : '...'}
               `}
-          description={user.email}
-        />
-      </List.Item>
-    )}
-  />);
+            description={user.email}
+          />
+        </List.Item>
+      )}
+    />
+  );
 };
