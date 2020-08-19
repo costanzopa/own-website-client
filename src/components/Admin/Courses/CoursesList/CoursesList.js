720226e0a3d6472f9c685a70e772b71fc74ec924
costanzopa <costanzopa@gmail.com>
2020-08-18 23:20:48 -0300

    Adding Courses list

--- src/components/Admin/Courses/CoursesList/CoursesList.js
+++ src/components/Admin/Courses/CoursesList/CoursesList.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from "react";
 import { List, Button, Modal as ModalAntd, notification } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import DragSortableList from "react-drag-sortable";
+
+import { getAccessTokenApi } from "../../../../api/auth";
+import {
+  getCourseDataUdemyApi,
+  deleteCourseApi,
+  updateCourseApi,
+} from "../../../../api/course";
 
 import "./CoursesList.scss";
+const { confirm } = ModalAntd;
 
 export default function CoursesList(props) {
   const { courses, setReloadCourses } = props;
@@ -15,7 +25,11 @@ export default function CoursesList(props) {
     courses.forEach((course) => {
       listCourseArray.push({
         content: (
-          <div>Course</div>
+          <Course
+            course={course}
+            deleteCourse={deleteCourse}
+            editCourseModal={() => console.log("dss")}
+          />
         ),
       });
     });
@@ -23,5 +37,107 @@ export default function CoursesList(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [courses]);
 
-  return (<div>CoursesList</div>);
+  const onSort = (sortedList, dropEvent) => {
+    const accessToken = getAccessTokenApi();
+
+    sortedList.forEach((item) => {
+      const { _id } = item.content.props.course;
+      const order = item.rank;
+      updateCourseApi(accessToken, _id, { order });
+    });
+  };
+
+  const deleteCourse = (course) => {
+    const accesToken = getAccessTokenApi();
+
+    confirm({
+      title: "Eliminando curso",
+      content:
+        `¿Estas seguro de que quieres eliminar el curso ${course.idCourse}?`,
+      okText: "Eliminar",
+      okType: "danger",
+      cancelText: "Cancelar",
+      onOk() {
+        deleteCourseApi(accesToken, course._id)
+          .then((response) => {
+            const typeNotification = response.code === 200
+              ? "success"
+              : "warning";
+            notification[typeNotification]({
+              message: response.message,
+            });
+            setReloadCourses(true);
+          })
+          .catch(() => {
+            notification["error"]({
+              message: "Error del servidor, intentelo más tarde.",
+            });
+          });
+      },
+    });
+  };
+
+  return (<div className="courses-list">
+    <div className="courses-list__header">
+      <Button
+        type="primary"
+        onClick={() => {
+          console.log();
+        }}
+      >
+        Nuevo curso
+      </Button>
+    </div>
+    <div className="courses-list__items">
+      {listCourses.length === 0 && (
+        <h2 style={{ textAlign: "center", margin: 0 }}>
+          No tienes cursos creados
+        </h2>
+      )}
+      <DragSortableList items={listCourses} onSort={onSort} type="vertical" />
+    </div>
+  </div>);
+}
+
+function Course(props) {
+  const { course, deleteCourse, editCourseModal } = props;
+  const [courseData, setCourseData] = useState(null);
+
+  useEffect(() => {
+    getCourseDataUdemyApi(course.idCourse).then((response) => {
+      if (response.code !== 200) {
+        notification["warning"]({
+          message: `El curso con el id ${course.idCourse} no se ha encontrado.`,
+        });
+      }
+      setCourseData(response.data);
+    });
+  }, [course]);
+
+  if (!courseData) {
+    return null;
+  }
+
+  return (
+    <List.Item
+      actions={[
+        <Button type="primary" onClick={() => editCourseModal(course)}>
+          <EditOutlined />
+        </Button>,
+        <Button type="danger" onClick={() => deleteCourse(course)}>
+          <DeleteOutlined />
+        </Button>,
+      ]}
+    >
+      <img
+        src={courseData.image_480x270}
+        alt={courseData.title}
+        style={{ width: "100px", marginRight: "20px" }}
+      />
+      <List.Item.Meta
+        title={`${courseData.title} | ID: ${course.idCourse}`}
+        description={courseData.headline}
+      />
+    </List.Item>
+  );
 }
