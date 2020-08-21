2dd9816186a5376765b3d2d05a6a761d6ae694ac
costanzopa <costanzopa@gmail.com>
2020-08-21 18:18:32 -0300

    Adding pagination

--- src/pages/Admin/Blog/Blog.js
+++ src/pages/Admin/Blog/Blog.js
@@ -1,27 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, notification } from "antd";
+import { withRouter } from "react-router-dom";
+import queryString from "query-string";
 import Modal from "../../../components/Modal";
+import PostsList from "../../../components/Admin/Blog/PostsList";
+import Pagination from "../../../components/Pagination";
+import AddEditPostForm from "../../../components/Admin/Blog/AddEditPostForm";
+import { getPostsApi } from "../../../api/post";
 
 import "./Blog.scss";
 
 const Blog = (props) => {
+  const { location, history } = props;
+  const [posts, setPosts] = useState(null);
+  const [reloadPosts, setReloadPosts] = useState(false);
   const [isVisibleModal, setIsVisibleModal] = useState(false);
   const [modalTitle, setModalTitle] = useState("");
   const [modalContent, setModalContent] = useState(null);
-  return (<div className="blog">
-    <div className="blog__add-post">
-      <Button type="primary" onClick={() => console.log("Add Post")}>
-        Nuevo post
-      </Button>
+  const { page = 1 } = queryString.parse(location.search);
+
+  useEffect(() => {
+    getPostsApi(12, page)
+      .then((response) => {
+        if (response?.code !== 200) {
+          notification["warning"]({
+            message: response.message,
+          });
+        } else {
+          setPosts(response.posts);
+        }
+      })
+      .catch(() => {
+        notification["error"]({
+          message: "Error del servidor.",
+        });
+      });
+    setReloadPosts(false);
+  }, [page, reloadPosts]);
+
+  const addPost = () => {
+    setIsVisibleModal(true);
+    setModalTitle("Creando nuevo post");
+    setModalContent(
+      <AddEditPostForm
+        setIsVisibleModal={setIsVisibleModal}
+        setReloadPosts={setReloadPosts}
+        post={null}
+      />,
+    );
+  };
+
+  const editPost = (post) => {
+    setIsVisibleModal(true);
+    setModalTitle("Editar post");
+    setModalContent(
+      <AddEditPostForm
+        setIsVisibleModal={setIsVisibleModal}
+        setReloadPosts={setReloadPosts}
+        post={post}
+      />,
+    );
+  };
+
+  if (!posts) {
+    return null;
+  }
+
+  return (
+    <div className="blog">
+      <div className="blog__add-post">
+        <Button type="primary" onClick={addPost}>
+          Nuevo post
+        </Button>
+      </div>
+      <PostsList
+        posts={posts}
+        setReloadPosts={setReloadPosts}
+        editPost={editPost}
+      />
+      <Pagination posts={posts} location={location} history={history} />
+      <Modal
+        title={modalTitle}
+        isVisible={isVisibleModal}
+        setIsVisible={setIsVisibleModal}
+        width="75%"
+      >
+        {modalContent}
+      </Modal>
     </div>
-    <Modal
-      title={modalTitle}
-      isVisible={isVisibleModal}
-      setIsVisible={setIsVisibleModal}
-      width="75%"
-    >
-      {modalContent}
-    </Modal>
-  </div>);
+  );
 };
-export default Blog;
+export default withRouter(Blog);
