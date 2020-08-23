0db27741cf8102f2222f958ea256188d085ec536
costanzopa <costanzopa@gmail.com>
2020-08-22 23:00:25 -0300

    Adding backend url

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,2 @@
-export const basePath = 'http://localhost:8080/api';
-export const apiVersion = 'v1';
+export const basePath = process.env.BACKEND_HOST || "http://localhost:8080/api";
+export const apiVersion = "v1";
