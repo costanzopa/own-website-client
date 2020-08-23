3f7d16dd07f149a2b2181a54c39bd768e0343059
costanzopa <costanzopa@gmail.com>
2020-08-22 23:33:54 -0300

    Adding backend url

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,4 @@
-export const basePath = process.env.BACKEND_HOST || "http://localhost:8080/api";
+export const basePath = (process.env.NODE_ENV === "production")
+  ? "https://own-website-api-live.herokuapp.com/api"
+  : "http://localhost:8080/api";
 export const apiVersion = "v1";
